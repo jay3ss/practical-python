23071f48b13193d094c0380de966478650d60322
jay ess <mentoce@gmail.com>
2024-03-22 16:14:17 -0700

    Implement exercie 1.9 (mortgage.py)

--- work/mortgage.py
+++ work/mortgage.py
@@ -19,6 +19,20 @@
 
 # When you run the new program, it should report a total payment of 929,965.62 over 342 months
 
+# Exercise 1.9: Making an Extra Payment Calculator
+
+# Modify the program so that extra payment information can be more generally handled. Make
+# it so that the user can set these variables:
+
+# extra_payment_start_month = 61
+# extra_payment_end_month = 108
+# extra_payment = 1000
+
+# Make the program look at these variables and calculate the total paid appropriately.
+
+# How much will Dave pay if he pays an extra $1000/month for 4 years starting after the first
+# five years have already been paid?
+
 principal = 500000.0
 rate = 0.05
 payment = 2684.11
@@ -26,12 +40,17 @@ total_paid = 0.0
 
 months = 0
 
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+
 while principal > 0:
     months += 1
-    if months <= 12:
-        addl_payment = 1000
+    if extra_payment_start_month <= months <= extra_payment_end_month:
+        addl_payment = extra_payment
     else:
         addl_payment = 0
+
     principal = principal * (1 + rate / 12) - payment - addl_payment
     total_paid = total_paid + payment + addl_payment
 
