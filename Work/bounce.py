7c6cec767666ec8cde356d237c3240479133e99a
jay ess <mentoce@gmail.com>
2024-03-22 15:11:48 -0700

    Implement exercies 1.5 (bounce.py)

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,44 @@
 # bounce.py
 #
 # Exercise 1.5
+
+# Exercise 1.5: The Bouncing Ball
+
+# A rubber ball is dropped from a height of 100 meters and each time it hits the
+# ground, it bounces back up to 3/5 the height it fell. Write a program bounce.py
+# that prints a table showing the height of the first 10 bounces.
+
+# Your program should make a table that looks something like this:
+
+# 1 60.0
+# 2 36.0
+# 3 21.599999999999998
+# 4 12.959999999999999
+# 5 7.775999999999999
+# 6 4.6655999999999995
+# 7 2.7993599999999996
+# 8 1.6796159999999998
+# 9 1.0077695999999998
+# 10 0.6046617599999998
+
+# Note: You can clean up the output a bit if you use the round() function. Try
+# using it to round the output to 4 digits.
+
+# 1 60.0
+# 2 36.0
+# 3 21.6
+# 4 12.96
+# 5 7.776
+# 6 4.6656
+# 7 2.7994
+# 8 1.6796
+# 9 1.0078
+# 10 0.6047
+height = 100  # height in meters
+bounce_ratio = 0.60
+bounce = 0
+
+while bounce < 10:
+    bounce += 1
+    height *= bounce_ratio
+    print(bounce, round(height, 4))
